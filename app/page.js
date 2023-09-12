bc2fcfbaf96d20aee68474ed031a4750686b8229
Yash-Tech13 <yashchandre013@gmail.com>
2023-09-12 22:24:33 +0530

    Add functionality to add tasks.

--- app/page.js
+++ app/page.js
@@ -1,19 +1,37 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 
 const page = () => {
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
+  const [tasks, setTasks] = useState([]);
 
   const submitHandler = (elem) => {
     // Prevents page from reloading(form submitting) so that data is retained
     elem.preventDefault();
     console.log(title);
     console.log(desc);
+    setTasks([...tasks, { title, desc }]);
+    console.log(tasks);
     // reset the form
     setTitle("");
     setDesc("");
+  };
+
+  let renderTask = <h2>No tasks available.</h2>;
+  if (tasks.length > 0) {
+    renderTask = tasks.map((t, i) => {
+      return (
+        <li>
+          <div className="flex justify-between mb-5">
+            <h5 className="text-xl font-semibold">{t.title}</h5>
+            <h6 className="text-xl font-semibold">{t.desc}</h6>
+          </div>
+        </li>
+      );
+    });
   }
+
   return (
     <>
       <h1 className="bg-black text-white p-5 text-5xl">Todo List</h1>
@@ -24,7 +42,7 @@ const page = () => {
           placeholder="Enter Task"
           value={title}
           onChange={(elem) => {
-            setTitle(elem.target.value)
+            setTitle(elem.target.value);
             // console.log(title)
           }}
         />
@@ -34,7 +52,7 @@ const page = () => {
           placeholder="Enter Description"
           value={desc}
           onChange={(elem) => {
-            setDesc(elem.target.value)
+            setDesc(elem.target.value);
             // console.log(desc)
           }}
         />
@@ -42,6 +60,10 @@ const page = () => {
           Add Task
         </button>
       </form>
+      <hr />
+      <div className="p-8 bg-slate-300">
+        <ul>{renderTask}</ul>
+      </div>
     </>
   );
 };
